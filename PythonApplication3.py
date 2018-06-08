7974ff03ca4db2a27eea4c0c3c0f842b4a97a57b
@raminta.zurauskaite <raminta.zurauskaite@stud.vgtu.lt>
2018-06-08 13:39:04 +0300

    button_grid_and_function

--- PythonApplication3.py
+++ PythonApplication3.py
@@ -1,9 +1,19 @@
 from tkinter import *
 from tkinter import ttk
 
+#root window
 root = Tk()
 root.title("To do list")
 
+#functions
+def add_task():
+    pass
+
+
+#variables
+task_name = StringVar()
+
+#mainframe
 mainframe = ttk.Frame(root, padding="0 1 1 0")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 mainframe.columnconfigure(0, weight=1)
@@ -13,10 +23,11 @@ n = ttk.Notebook(mainframe)
 f1 = ttk.Frame(n)        
 n.add(f1, text='List of tasks')
 
-task_name = StringVar()
 
+#text input
 txt_input = ttk.Entry(f1,width=40, textvariable=task_name)
-
+#button for text input
 btn_add_task = ttk.Button(f1, text='Add task', command = add_task)
+btn_add_task.grid(column=1, columnspan=2, row= 1)
 
 root.mainloop()
