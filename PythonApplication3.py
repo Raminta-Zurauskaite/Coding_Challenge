6280367d10ff9710dbc0a0dc6094ade029a99496
@raminta.zurauskaite <raminta.zurauskaite@stud.vgtu.lt>
2018-06-08 13:06:43 +0300

    addition_of_mainframe

--- PythonApplication3.py
+++ PythonApplication3.py
@@ -1,5 +1,16 @@
 from tkinter import *
 from tkinter import ttk
 
+root = Tk()
+root.title("To do list")
+
+mainframe = ttk.Frame(root, padding="0 1 1 0")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+mainframe.columnconfigure(0, weight=1)
+mainframe.rowconfigure(0, weight=1)
+root.resizable(False, False)
+n = ttk.Notebook(mainframe)
+f1 = ttk.Frame(n)        
+n.add(f1, text='List of tasks')
 
 root.mainloop()
