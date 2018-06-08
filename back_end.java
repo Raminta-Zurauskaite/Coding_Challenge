d7c4f607af84d1fec426697ea52429f0ade94370
@raminta.zurauskaite <raminta.zurauskaite@stud.vgtu.lt>
2018-06-08 12:53:15 +0300

    Revert "back_end_v0"
    This reverts commit 96ad8421b51f5a826b9f9f5a9958972253022888.

--- back_end.java
+++
@@ -1,8 +0,0 @@
-package components;
-
-    public ToDoList() {
-        super(new BorderLayout());
- 
-        listModel = new DefaultListModel();
-        listModel.addElement("Go to the grocery store");
-        listModel.addElement("Clean the house");
